6a54d2b179cbc302ba33c49ea105571264b4785b
Joseph Awad <josephawad3@gmail.com>
2024-02-16 20:39:33 +0200

    Create info route and add vscode/settings

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Holy Bible API'
+@app.route('/info')
+def get_info():
+  info_data = {
+    "name": "Holy Bible API",
+    "version": "1.0.0",
+    "translations": ["American Standard Version (1901)", "King James Version"]
+  }
+  return jsonify(info_data)
+
+#if __name__ == '__main__':
+#  app.run(debug=True)
